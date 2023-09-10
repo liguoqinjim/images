c5a50b580507919da0926c3c9ffbd3986759de78
liguoqinjim <>
2023-09-10 22:39:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# images
+![GitHub repo size](https://img.shields.io/github/repo-size/liguoqinjim/images)
---
+++ scripts/script001.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rsync -avut /Volumes/docker/my_images/ /Users/li/Workspace/github.com/images/
